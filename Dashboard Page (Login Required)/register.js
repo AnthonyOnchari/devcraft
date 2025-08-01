3f8843a6a481e455f910b92e6185ddd5f1507b40
Tonie 404 <info.onchari@gmail.com>
2025-08-01 13:20:04 +0000

    Register Initial

---
+++ Dashboard Page (Login Required)/register.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDA7MWciYaB12AizympVc-UAaCRj-0I3UY",
+  authDomain: "devcraft-1.firebaseapp.com",
+  projectId: "devcraft-1",
+  storageBucket: "devcraft-1.firebasestorage.app",
+  messagingSenderId: "730800879156",
+  appId: "1:730800879156:web:f4b7758acc920273dbbc61"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
